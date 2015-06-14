0b3f74a59cdd69e1f73b900c549fd9caf948636e
kendyhikaru <kendy_hikaru@yahoo.com.vn>
2015-06-14 22:41:14 +0700

    Add Script
    Script for Expoit template

---
+++ Scripts/blind_sql.py
@@ -0,0 +1,14 @@
+import re
+import requests
+__author__ = 'namhb'
+
+class SQLExploit:
+	def __init__(self):
+		print("[+] Blind SQL injection Exploit")
+		self.PHPSESSID = ""
+		self.url = ""
+
+	def check():
+		cookie = dict(
+			PHPSESSID =	self.PHPSESSID
+		)
