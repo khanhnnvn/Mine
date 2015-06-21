42f56f1e7e808d9194e34c0eeab043501cff9f48
kendyhikaru <Trang137>
2015-06-21 17:17:24 +0700

    Add UserAgent

--- Scripts/blind_sql.py
+++ Scripts/blind_sql.py
@@ -7,6 +7,7 @@ class SQLExploit:
         print("[+] Blind SQL injection Exploit")
         self.PHPSESSID = ""
         self.url = ""
+        self.userAgent = "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20071008 Ubuntu/7.10 (gutsy) Firefox/2.0.0.6"
 
     def check():
             cookie = dict(
