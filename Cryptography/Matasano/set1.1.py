61b1395d1be21c918edbcca847a1b2436e477899
kendyhikaru <kendy_hikaru@yahoo.com.vn>
2016-04-28 02:04:46 +0000

    Restruct all git

--- Cryptography/Matasano/set1.1.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'namhb1'
-
-import binascii, base64
-str 		=	"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-hexStr 		=	binascii.unhexlify(str)
-b64Str 		=	base64.b64encode(hexStr)
-
-print("Result: {}".format(b64Str))
